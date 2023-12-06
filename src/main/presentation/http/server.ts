883cfc3a170dcccc7544bea89ff96ed5ed4a46f2
Werik Jahel <werikjahel@gmail.com>
2023-12-05 21:33:16 -0300

    aula-28_05/12/23_21:33

--- src/main/presentation/http/server.ts
+++ src/main/presentation/http/server.ts
@@ -1,19 +1,3 @@
-import http from 'node:http';
-
-const createHTTPServer = async (): Promise<http.Server> => {
-    const httpServer: http.Server = http.createServer(
-        function (request, response) {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write('<html><body><p>Hello, Servidor Web com Node.js</p></body></html>');
-            response.end();
-        }
-    );
-    return httpServer;
-};
-
-export { createHTTPServer }
-
-/*
 import express, { Application } from 'express';
 import http from 'node:http';
 import { apiv1Router } from './rest/api.v1';
@@ -33,4 +17,3 @@ export const createHTTPServer = async (): Promise<http.Server> => {
 
     return httpServer;
 }
-*/
