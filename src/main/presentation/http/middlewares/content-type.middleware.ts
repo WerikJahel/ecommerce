1c0a31ceaa7b0885140f6972e50252190c09e7f2
Werik Jahel <werikjahel@gmail.com>
2024-01-15 21:25:56 -0300

    Parte 34

--- src/main/presentation/http/middlewares/content-type.middleware.ts
+++ src/main/presentation/http/middlewares/content-type.middleware.ts
@@ -1,16 +1,14 @@
+import { HttpErrors } from "@shared/presentation/http/http.error";
 import { NextFunction, Request, Response } from "express";
 
 const allowedContentTypes = ['application/json'];
 
-function contentTypeMiddleware(
-    request: Request,
-    response: Response,
-    next: NextFunction
-) {
+const contentTypeMiddleware = (request: Request, response: Response, next: NextFunction) => {
+
     const contentType = request.headers['content-type'];
 
     if (!contentType || !allowedContentTypes.includes(contentType)) {
-        return response.status(415).send('Tipo de Mídia Não Suportado');
+        next(new HttpErrors.UnsupportedMediaTypeError());
     }
 
     next();
